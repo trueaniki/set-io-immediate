ffd3b375f43d3f6663b203a5ff3a1c16f2bf56ed
Aniki <anikipelau@gmail.com>
2024-03-30 01:13:45 +0300

    add example

---
+++ examples/example.js
@@ -0,0 +1,55 @@
+const { setIoImmediate, setUvImmediate } = require('../build/Release/set-io-immediate.node');
+const asyncHooks = require('async_hooks');
+const fs = require('fs');
+
+const log = (...args) => {
+  fs.writeSync(1, args.join(' ') + '\n');
+}
+
+const resources = {};
+
+const hook = asyncHooks.createHook({
+  init(asyncId, type, triggerAsyncId, resource) {
+    resources[asyncId] = { type, triggerAsyncId };
+    log('init', asyncId, type, triggerAsyncId);
+  },
+  before(asyncId) {
+    log('before', resources[asyncId].type, asyncId, resources[asyncId].triggerAsyncId);
+  },
+  after(asyncId) {
+    log('after', resources[asyncId].type, asyncId, resources[asyncId].triggerAsyncId);
+  },
+  destroy(asyncId) {
+    log('destroy', resources[asyncId].type, asyncId, resources[asyncId].triggerAsyncId);
+  }
+});
+
+console.log = log;
+hook.enable();
+
+process.nextTick(() => {
+  console.log('nextTick')
+})
+
+Promise.resolve().then(() => {
+  console.log('promise')
+})
+
+setImmediate(() => {
+  console.log('immediate')
+})
+
+setIOImmediate.withNan(
+  () => {
+    console.log('nan worker finished')
+  },
+  () => {
+    console.log('nan worker started')
+  }
+)
+
+console.log('sync')
+
+setTimeout(() => {
+  console.log('timeout')
+}, 0)
